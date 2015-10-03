180cb186403fc271331cf224eedba221a5a969ce
Marc Clifton <marc.clifton@gmail.com>
2015-10-03 15:32:59 -0400

    Removed drawing and Image references.

--- Clifton.ExtensionMethods/ExtensionMethods.cs
+++ Clifton.ExtensionMethods/ExtensionMethods.cs
@@ -3,10 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Drawing.Imaging;
+// using System.Drawing;
+// using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -365,7 +366,7 @@ namespace Clifton.ExtensionMethods
 		{
 			return s == String.Empty;
 		}
-
+#if INCLUDE_DRAWING
 		public static Image Resize(this Image image, int maxWidth)
 		{
 			// To encode as JPG, see http://stackoverflow.com/questions/10894836/c-sharp-convert-image-formats-to-jpg
@@ -424,6 +425,14 @@ namespace Clifton.ExtensionMethods
 			return base64String;
 		}
 
+		/// Returns just the RGB component of a color.
+		public static int ToRgb(this Color color)
+		{
+			return color.ToArgb() & 0x00FFFFFF;
+		}
+
+#endif
+
 		/// <summary>
 		/// Return the smaller of the two values.
 		/// </summary>
@@ -438,12 +447,6 @@ namespace Clifton.ExtensionMethods
 		public static int Larger(this int val, int otherVal)
 		{
 			return val > otherVal ? val : otherVal;
-		}
-
-		/// Returns just the RGB component of a color.
-		public static int ToRgb(this Color color)
-		{
-			return color.ToArgb() & 0x00FFFFFF;
 		}
 
 		public static string DateFromPosix(this string s, string format = "MM-dd-yy")
@@ -917,6 +920,73 @@ namespace Clifton.ExtensionMethods
 		public static byte[] to_Utf8(this string str)
 		{
 			return Encoding.UTF8.GetBytes(str);
-		}			
+		}
+
+		// From here: http://www.codeproject.com/Articles/769741/Csharp-AES-bits-Encryption-Library-with-Salt
+		public static byte[] AES_Encrypt(this byte[] bytesToBeEncrypted, byte[] passwordBytes)
+		{
+			byte[] encryptedBytes = null;
+
+			// Set your salt here, change it to meet your flavor:
+			// The salt bytes must be at least 8 bytes.
+			byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+			using (MemoryStream ms = new MemoryStream())
+			{
+				using (RijndaelManaged AES = new RijndaelManaged())
+				{
+					AES.KeySize = 256;
+					AES.BlockSize = 128;
+
+					var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+					AES.Key = key.GetBytes(AES.KeySize / 8);
+					AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+					AES.Mode = CipherMode.CBC;
+
+					using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+					{
+						cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+						cs.Close();
+					}
+					encryptedBytes = ms.ToArray();
+				}
+			}
+
+			return encryptedBytes;
+		}
+
+		public static byte[] AES_Decrypt(this byte[] bytesToBeDecrypted, byte[] passwordBytes)
+		{
+			byte[] decryptedBytes = null;
+
+			// Set your salt here, change it to meet your flavor:
+			// The salt bytes must be at least 8 bytes.
+			byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+			using (MemoryStream ms = new MemoryStream())
+			{
+				using (RijndaelManaged AES = new RijndaelManaged())
+				{
+					AES.KeySize = 256;
+					AES.BlockSize = 128;
+
+					var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+					AES.Key = key.GetBytes(AES.KeySize / 8);
+					AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+					AES.Mode = CipherMode.CBC;
+
+					using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+					{
+						cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+						cs.Close();
+					}
+					decryptedBytes = ms.ToArray();
+				}
+			}
+
+			return decryptedBytes;
+		}
 	}
 }
