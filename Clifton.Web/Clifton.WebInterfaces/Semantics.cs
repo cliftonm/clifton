201f4c8d990fe4295462f3c1a8e19fcd409fce9a
Marc Clifton <marc.clifton@gmail.com>
2015-11-28 11:15:07 -0500

    Project restructuring, take 3!

---
+++ Clifton.Web/Clifton.WebInterfaces/Semantics.cs
@@ -0,0 +1,17 @@
+﻿using Clifton.Core.Semantics;
+
+namespace Clifton.WebInterfaces
+{
+	public class SocketMembrane : Membrane { }
+
+	public class ServerSocketMessage : ISemanticType
+	{
+		public IWebSocketSession Session { get; set; }
+		public string Text { get; set; }
+	}
+
+	public class ClientSocketMessage : ISemanticType
+	{
+		public string Text { get; set; }
+	}
+}
