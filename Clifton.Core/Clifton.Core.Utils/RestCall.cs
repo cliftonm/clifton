a51459e00d77141ef40099561b9cb1df2d2dd94b
Marc Clifton <marc.clifton@gmail.com>
2015-11-28 17:39:46 -0500

    Routing service, response service, and adding REST get/post calls to utils.

---
+++ Clifton.Core/Clifton.Core.Utils/RestCall.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Clifton.Core.Utils
+{
+	public static class RestCall
+	{
+		public static string Get(string url)
+		{
+			string ret = String.Empty;
+			WebResponse resp = null;
+
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			request.Method = "GET";
+
+			resp = request.GetResponse();
+			ret = new StreamReader(resp.GetResponseStream()).ReadToEnd();
+			resp.Close();
+
+			return ret;
+		}
+
+		public static R Post<R>(string url, object obj)
+		{
+			R target = Activator.CreateInstance<R>();
+			Stream st = null;
+			string json = string.Empty;
+			string retjson = string.Empty;
+
+			json = JsonConvert.SerializeObject(obj);
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			request.Method = "POST";
+			request.ContentType = "application/json";
+			request.ContentLength = json.Length;
+			st = request.GetRequestStream();
+			byte[] bytes = Encoding.UTF8.GetBytes(json);
+			st.Write(bytes, 0, bytes.Length);
+			WebResponse resp = request.GetResponse();
+			retjson = new StreamReader(resp.GetResponseStream()).ReadToEnd();
+			JObject jobj = JObject.Parse(retjson);
+			JsonConvert.PopulateObject(jobj.ToString(), target);
+
+			resp.Close();
+			st.Close();
+
+			return target;
+		}
+	}
+}
---
+++ Clifton.Web/Clifton.WebRouterService/PublicRouterReceptor.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using Clifton.Core.ExtensionMethods;
+using Clifton.Core.Semantics;
+using Clifton.Core.ServiceManagement;
+using Clifton.WebInterfaces;
+
+namespace Clifton.WebRouterService
+{
+	/// <summary>
+	/// Maps a route to the semantic type that processes that route.
+	/// The route key is the verb and path, the route dictionary value is the semantic type to instantiate
+	/// for that route.  We assume here that the semantic type will be populated from deserialized JSON data
+	/// that is in the request input stream.
+	/// If the route key is not found in the web service route dictionary, then the UnhandledContext semantic
+	/// type is instantiated on the WebServerMembrane bus.  This is (default) handled in the WebFileResponseService module,
+	/// by the UnhandledContext receptor.
+	/// </summary>
+	public class PublicRouterReceptor : IReceptor
+	{
+		public void Process(ISemanticProcessor proc, IMembrane membrane, Route route)
+		{
+			IPublicRouterService routerService = proc.ServiceManager.Get<IPublicRouterService>();
+			HttpListenerContext context = route.Context;
+			string data = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding).ReadToEnd();
+			HttpVerb verb = context.Verb();
+			UriPath path = context.Path();
+			string searchRoute = GetSearchRoute(verb, path);
+			RouteInfo routeInfo;
+
+			// Semantic routes can be either public or authenticated.
+			if (routerService.Routes.TryGetValue(searchRoute, out routeInfo))
+			{
+				Type receptorSemanticType = routeInfo.ReceptorSemanticType;
+				SemanticRoute semanticRoute = (SemanticRoute)Activator.CreateInstance(receptorSemanticType);
+				semanticRoute.PostData = data;
+
+				if (!String.IsNullOrEmpty(data))
+				{
+					// Is it JSON?
+					if (data[0] == '{')
+					{
+						JsonConvert.PopulateObject(data, semanticRoute);
+					}
+					else
+					{
+						// Example: "username=sdfsf&password=sdfsdf&LoginButton=Login"
+						string[] parms = data.Split('&');
+
+						foreach (string parm in parms)
+						{
+							string[] keyVal = parm.Split('=');
+							PropertyInfo pi = receptorSemanticType.GetProperty(keyVal[0], BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+
+							if (pi != null)
+							{
+								// TODO: Convert to property type.
+								// TODO: value needs to be re-encoded to handle special characters.
+								pi.SetValue(semanticRoute, keyVal[1]);
+							}
+						}
+					}
+				}
+				else if (verb.Value == "GET")
+				{
+					// Parse parameters
+					NameValueCollection nvc = context.Request.QueryString;
+
+					foreach(string key in nvc.AllKeys)
+					{
+						PropertyInfo pi = receptorSemanticType.GetProperty(key, BindingFlags.Public | BindingFlags.Instance);
+
+						if (pi != null)
+						{
+							// TODO: Convert to property type.
+							// TODO: value needs to be re-encoded to handle special characters.
+							pi.SetValue(semanticRoute, nvc[key]);
+						}
+					}
+				}
+
+				// Must be done AFTER populating the object -- sometimes the json converter nulls the base class!
+				semanticRoute.Context = context;
+				proc.ProcessInstance<WebServerMembrane>(semanticRoute, true);
+			}
+			else
+			{
+				// Put the context on the bus for some service to pick up.
+				// All unhandled context are assumed to be public routes.
+				proc.ProcessInstance<WebServerMembrane, UnhandledContext>(c => c.Context = context);
+			}
+		}
+
+		protected string GetSearchRoute(HttpVerb verb, UriPath path)
+		{
+			return verb.Value + ":" + path.Value;
+		}
+	}
+}
