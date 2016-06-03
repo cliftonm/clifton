320a8fadd14426d7945d359bc9a47198874eb999
Marc Clifton <marc.clifton@gmail.com>
2016-06-03 13:09:26 -0400

    BeginInvoke and Invoke Form extension methods.

---
+++ Clifton.Core/Clifton.Core.ExtensionMethods/WinFormExtensionMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Clifton.Core.ExtensionMethods
+{
+	public static class WinFormExtensionMethods
+	{
+		/// <summary>
+		/// Asynchronous invoke on application thread.  Will return immediately unless invocation is not required.
+		/// </summary>
+		public static void BeginInvoke(this Control control, Action action)
+		{
+			if (control.InvokeRequired)
+			{
+				// We want a synchronous call here!!!!
+				control.BeginInvoke((Delegate)action);
+			}
+			else
+			{
+				action();
+			}
+		}
+
+		/// <summary>
+		/// Synchronous invoke on application thread.  Will not return until action is completed.
+		/// </summary>
+		public static void Invoke(this Control control, Action action)
+		{
+			if (control.InvokeRequired)
+			{
+				// We want a synchronous call here!!!!
+				control.Invoke((Delegate)action);
+			}
+			else
+			{
+				action();
+			}
+		}
+	}
+}
