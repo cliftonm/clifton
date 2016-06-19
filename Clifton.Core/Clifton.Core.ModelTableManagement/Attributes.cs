0427c27d79c23c00fc95fd9b83dd3f40d71d75d9
Marc Clifton <marc.clifton@gmail.com>
2016-06-19 18:51:11 -0400

    Added View attribute to indicate, in the data context, which tables are actually views and not to be created or treated as models with a backing table.

--- Clifton.Core/Clifton.Core.ModelTableManagement/Attributes.cs
+++ Clifton.Core/Clifton.Core.ModelTableManagement/Attributes.cs
@@ -6,6 +6,11 @@ namespace Clifton.Core.ModelTableManagement
 	public class UniqueAttribute : Attribute { }
 	public class ReadOnlyAttribute : Attribute { }
 
+	/// <summary>
+	/// Add View attribute to the data context to indicate that a Table is a view and should not be created in the database.
+	/// </summary>
+	public class ViewAttribute : Attribute { }			
+
 	public class DisplayNameAttribute : Attribute
 	{
 		public string DisplayName { get; set; }
