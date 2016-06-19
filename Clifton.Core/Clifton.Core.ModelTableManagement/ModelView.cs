e5c1bd99b74793a06ba0e73834b449a53eaa0f93
Marc Clifton <marc.clifton@gmail.com>
2016-06-19 18:53:29 -0400

    Added a ModelView, which is a view not backed by a physical table, so changes don't persist.

---
+++ Clifton.Core/Clifton.Core.ModelTableManagement/ModelView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+using Clifton.Core.ExtensionMethods;
+using Clifton.Core.ServiceInterfaces;
+
+namespace Clifton.Core.ModelTableManagement
+{
+	/// <summary>
+	/// Views do not transact with the database, so the insert, delete, and update methods do nothing.
+	/// </summary>
+	public class ModelView<T> : ModelTable<T> where T : MappedRecord, IEntity, new()
+	{
+		public ModelView(ModelMgr modelMgr, IDbContextService db, DataTable backingTable, List<IEntity> modelCollection) :
+			base(modelMgr, db, backingTable, modelCollection)
+		{
+		}
+
+		protected override void Insert(T newInstance)
+		{
+		}
+
+		protected override void Delete(IEntity item)
+		{
+		}
+
+		protected override void Update(IEntity instance)
+		{
+		}
+	}
+}
