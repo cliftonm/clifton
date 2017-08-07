1192dad053b3b1ed3c903857bf995c501a98a5d8
cliftonm <marc.clifton@gmail.com>
2017-08-07 13:20:15 -0400

    Example client and server web socket.

---
+++ Clifton.Web/Clifton.WebSocketService/Example/websocketClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Clifton.Core.Semantics;
+using Clifton.Core.Services.SemanticProcessorService;
+using Clifton.Core.ServiceManagement;
+using Clifton.Core.Web.WebSocketService;
+using Clifton.WebInterfaces;
+
+namespace websocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceManager sm = new ServiceManager();
+
+            SemanticProcessor semproc = new SemanticProcessor();
+            semproc.Initialize(sm);
+            sm.RegisterSingleton<ISemanticProcessor>(semproc);
+
+            var wsClient = new WebSocketClientService();
+            wsClient.Initialize(sm);
+            sm.RegisterSingleton<IWebSocketClientService>(wsClient);
+
+            semproc.Register<SocketMembrane, ServerSocketReceiver>();
+
+            wsClient.Start("ws://127.0.0.1", 1000, "/wstest");
+
+            Console.WriteLine("WS client connected to 127.0.0.1:1000");
+            wsClient.Send("Hello World!");
+            Console.ReadLine();
+        }
+    }
+
+    public class ServerSocketReceiver : IReceptor
+    {
+        public void Process(ISemanticProcessor pool, IMembrane membrane, ClientSocketMessage msg)
+        {
+            Console.WriteLine("Received " + msg.Text);
+        }
+    }
+}
---
+++ Clifton.Web/Clifton.WebSocketService/Example/websocketServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Clifton.Core.Semantics;
+using Clifton.Core.Services.SemanticProcessorService;
+using Clifton.Core.ServiceManagement;
+using Clifton.Core.Web.WebSocketService;
+using Clifton.WebInterfaces;
+
+namespace websocketServer
+{
+    class Program
+    {
+        public static WebSocketServerService wsServer;
+
+        static void Main(string[] args)
+        {
+            ServiceManager sm = new ServiceManager();
+
+            SemanticProcessor semproc = new SemanticProcessor();
+            semproc.Initialize(sm);
+            sm.RegisterSingleton<ISemanticProcessor>(semproc);
+
+            wsServer = new WebSocketServerService();
+            wsServer.Initialize(sm);
+            sm.RegisterSingleton<IWebSocketServerService>(wsServer);
+
+            semproc.Register<SocketMembrane, ServerSocketReceiver>();
+            wsServer.Start("127.0.0.1", 1000, "/wstest");
+
+            Console.WriteLine("WS listening on 127.0.0.1:1000");
+            Console.ReadLine();
+            wsServer.Stop();
+        }
+    }
+
+    public class ServerSocketReceiver : IReceptor
+    {
+        public void Process(ISemanticProcessor pool, IMembrane membrane, ServerSocketMessage msg)
+        {
+            Console.WriteLine("Received " + msg.Text);
+            msg.Session.Reply("How are you?");
+        }
+    }
+}
